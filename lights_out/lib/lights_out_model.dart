58e159f29e0b8c742686afec9bee0d28da1e4502
Joseph Peters <petersjl@rose-hulman.edu>
2025-05-05 11:44:35 -0400

    Move game logic to model

--- lights_out/lib/lights_out_model.dart
+++ lights_out/lib/lights_out_model.dart
@@ -1,9 +1,16 @@
+import 'dart:math';
+
 class LightsOutModel {
   final int lights;
   int clicks = 0;
   List<bool> grid;
 
-  LightsOutModel(this.lights) : grid = List.generate(lights, (_) => false);
+  LightsOutModel(this.lights) : grid = List.generate(lights, (_) => false) {
+    _randomizeLights();
+    clicks = 0; // Reset clicks after randomization
+  }
+
+  bool get gameWon => grid.every((light) => !light);
 
   bool getLightState(int index) {
     return grid[index];
@@ -14,5 +21,12 @@ class LightsOutModel {
     grid[index] = !grid[index];
     if (index > 0) grid[index - 1] = !grid[index - 1]; // Left
     if (index < lights - 1) grid[index + 1] = !grid[index + 1]; // Right
+  }
+
+  void _randomizeLights() {
+    for (int i = 0; i < lights; i++) {
+      toggleLight(Random().nextInt(lights));
+    }
+    clicks = 0; // Reset clicks after randomization
   }
 }
--- lights_out/lib/lights_out_view_model.dart
+++ lights_out/lib/lights_out_view_model.dart
@@ -1,20 +1,16 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:lights_out/lights_out_model.dart';
 
 class LightsOutViewModel extends ChangeNotifier {
-  int _lightsCount;
+  final int _lightsCount;
   LightsOutModel _model;
 
   LightsOutViewModel([this._lightsCount = 7])
-    : _model = LightsOutModel(_lightsCount) {
-    _randomizeLights();
-  }
+    : _model = LightsOutModel(_lightsCount);
 
   List<bool> get lights => _model.grid;
   int get clicks => _model.clicks;
-  bool get gameWon => _model.grid.every((light) => !light);
+  bool get gameWon => _model.gameWon;
 
   // Toggle a light and notify listeners
   void toggleLight(int index) {
@@ -23,15 +19,7 @@ class LightsOutViewModel extends ChangeNotifier {
   }
 
   void newGame() {
-    _model = LightsOutModel(this._lightsCount);
-    _randomizeLights();
+    _model = LightsOutModel(_lightsCount);
     notifyListeners();
-  }
-
-  void _randomizeLights() {
-    for (int i = 0; i < _model.lights; i++) {
-      _model.toggleLight(Random().nextInt(_model.lights));
-    }
-    _model.clicks = 0; // Reset clicks after randomization
   }
 }
